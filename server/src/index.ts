3a7636eb33c8f228b83682648a7eb48815673a0c
John <wheeler6123@gmail.com>
2023-06-16 17:59:47 -0600

    added controllers, added cards array to Deck model, and added /card endpoint for adding cards to deck

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,30 +3,19 @@ import config from '../config/config';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import Deck from './models/Deck';
+import { getDecksController } from './controllers/getDecksController';
+import { createDeckController } from './controllers/createDeckController';
+import { deleteDeckController } from './controllers/deleteDeckController';
+import { createCardForDeckController } from './controllers/createCardForDeckController';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/decks', async (req:Request, res:Response) => {
-    let decks = await Deck.find();
-    res.json(decks);
-    console.log(decks);
-});
-
-app.post('/decks', async (req:Request, res:Response) => {
-    const newDeck = new Deck({
-        title: req.body.title,
-    });
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck);
-});
-
-app.delete('/decks/:deckId', async (req:Request, res:Response) => {
-    const deckId = req.params.deckId;
-    const deletedDeck = await Deck.findByIdAndRemove(deckId);
-    res.json(deletedDeck);
-});
+app.get('/decks', getDecksController);
+app.post('/decks', createDeckController);
+app.delete('/decks/:deckId', deleteDeckController);
+app.post('/decks/:deckId/cards', createCardForDeckController);
 
 const connectDB = async () => {
     const connection = await mongoose.connect(config.MONGO_URI)
